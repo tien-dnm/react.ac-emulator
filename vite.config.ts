4a26a909ce6cb3e8a2972247433af86fe5bce95f
Đỗ Ngọc Minh Tiến <tien.dnm@outlook.com>
2023-08-03 12:57:57 +0000

    change project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react.ac-emulator/",
+  base: "/reactjs.ac-emulator/",
 });
